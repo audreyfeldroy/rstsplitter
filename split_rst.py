70e2f37240575946f96d05e1c28211d46df658de
Audrey Roy <audreyr@gmail.com>
2013-06-20 15:12:50 +0200

    Add RST splitter script.

---
+++ split_rst.py
@@ -0,0 +1,45 @@
+
+def is_all_equal_signs(s):
+	"""
+		Returns True if a string consists entirely of = signs
+	"""
+	for c in s:
+		if c != '=':
+			return False
+	return True
+	
+def is_chapter_header(text, line_num):
+	"""
+		Returns True if:
+		1. the current line is not longer than the next line
+		2. the next line consists of entirely = characters
+	"""
+	return (len(text[line_num]) > 0 and 
+		len(text[line_num]) <= len(text[line_num+1]) and
+		is_all_equal_signs(text[line_num+1]))
+
+def header_to_filename(header_string):
+	return header_string.lower().replace(' ', '_') + ".rst"
+
+input = open('huge_file.rst', 'r').read().split('\n')
+
+next_filename = ""
+next_file_content = []
+
+for line_num in range(0, len(input) - 1):
+	
+	if is_chapter_header(input, line_num):
+		
+		if next_filename:
+			
+			# Write out next_file_content
+			output = open(next_filename, 'w')
+			output.write('\n'.join(next_file_content))
+			output.close()
+			
+			# Clear the contents for the next file
+			next_file_content = []
+			
+		next_filename = header_to_filename(input[line_num])
+		
+	next_file_content.append(input[line_num])
